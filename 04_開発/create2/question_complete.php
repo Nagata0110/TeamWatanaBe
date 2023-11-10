0d5e581c182399c3c4991b6dc404fb675e681188
2101216 <115523355+2101216@users.noreply.github.com>
2023-11-10 12:28:12 +0900

    仮

--- 04_開発/create2/question_complete.php
+++ 04_開発/create2/question_complete.php
@@ -39,15 +39,17 @@
         </nav>
     </div>
     <div class="text-center">
-        <?php
-        // POSTリクエストを処理
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // フォームからデータを取得し、データベースに保存する処理を追加することができます。
-            echo '<h2 class="mt-5">問題の登録申請を行いました。</h2>';
-        }
-        ?>
-        <button class="mt-5 btn btn-primary" onclick="location.href='開発するときはパス指定で入れる。例：./question_create.htmlのように指定する'">ホームへ</button>
-    </div>
+    <?php
+    // POSTリクエストを処理
+    if ($_POST && isset($_POST['answer'])) {
+        // フォームからデータを取得し、データベースに保存する処理を追加することができます。
+        print '<h2 class="mt-5">問題の登録申請を行いました。</h2>';
+
+    }
+    ?>
+    <button class="mt-5 btn btn-primary" onclick="location.href='開発するときはパス指定で入れる。例：./question_create.htmlのように指定する'">ホームへ</button>
+</div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
