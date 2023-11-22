f17aaa30cc6138543d8acfa6e0cae0790fa650bf
Nagata0110 <2101188@s.asojuku.ac.jp>
2023-11-22 09:12:51 +0900

    修正

--- 04_開発/create/question_complete.php
+++ 04_開発/create/question_complete.php
@@ -31,13 +31,6 @@
         </nav>
     </div>
     <?php
-        $pdo = new PDO('mysql:host=mysql202.phy.lolipop.lan;dbname=LAA1418434-aaa;charset=utf8','LAA1418434', '090414');
-        $sqlLike ="SELECT * FROM quiz";
-        $ps1 = $pdo->prepare($sqlLike);
-        $ps1->execute();
-        $cnt = 0;
-        foreach($ps1 -> fetchAll() as $row){
-        if($row['question'] != $_POST['question'] && $row['expl'] != $_POST['expl'] && $cnt != 0){
             $sql = "INSERT INTO quiz(question, choices1, choices2, choices3, choices4, answer, expl, quiz_title, private) VALUES(?,?,?,?,?,?,?,?,?)";
             $ps = $pdo->prepare($sql);
             $ps->bindValue(1, $_POST['question'], PDO::PARAM_STR);
@@ -69,19 +62,6 @@
             <h2 class="mt-5">問題の登録申請を行いました。</h2>
             <button class="mt-5 btn btn-primary" onclick="location.href='./question_create.php'">問題登録へ</button>
         </div>
-        <?php
-        $cnt++;
-        }else if($cnt != 0){
-        ?>
-            <div class="text-center md-3">
-                <h2 class="mt-5">既に問題が登録されています。</h2>
-                <button class="mt-5 btn btn-primary" onclick="location.href='./question_create.php'">問題登録へ</button>
-            </div>
-        <?php
-        $cnt++;
-        }
-    }
-    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
